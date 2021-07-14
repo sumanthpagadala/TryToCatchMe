a35c9186c49e2fe25aa82c311e3bd10ee4b2730e
sumanthpagadala <64856837+sumanthpagadala@users.noreply.github.com>
2021-07-14 12:49:30 +0530

    Add files via upload

---
+++ code for you.py
@@ -0,0 +1,15 @@
+
+
+def keyword():
+    print(" Do you know what is bitly then go google it.... ")
+
+def promocode():
+    print(" Ok... I will make it simple for you use my zomato promocode for it: ")
+
+def code():
+    print("3r96ajp   I hope you get a chicken sandwich and a cheese burger...!")
+
+keyword()
+
+promocode()
+code()
